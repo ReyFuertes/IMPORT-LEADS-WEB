694a23597a421232cf5a62a77a6c426df165719e
RFuertes <reynelfuertes@gmail.com>
2020-02-27 21:41:43 +0800

    change route to fix deploy error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ const routes: Routes = [
   {
     path: 'dashboard',
     children: [
-      { path: 'contracts', loadChildren: () => import('./modules/contracts/contracts.module').then(m => m.ContractsModule) },
-      { path: 'inspection-insights', loadChildren: () => import('./modules/inspection-insights/inspection-insights.module').then(m => m.InspectionInsightsModule) },
-      { path: 'inspections', loadChildren: () => import('./modules/inspections/inspections.module').then(m => m.InspectionModule) },
-      { path: 'performance-insights', loadChildren: () => import('./modules/performance-insights/performance-insights.module').then(m => m.PerformanceInsightsModule) },
-      { path: 'tags', loadChildren: () => import('./modules/tags/tags.module').then(m => m.TagsModule) },
-      { path: 'templates', loadChildren: () => import('./modules/templates/templates.module').then(m => m.TemplatesModule) },
-      { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
-      { path: 'venues', loadChildren: () => import('./modules/venues/venues.module').then(m => m.VenuesModule) }
+      { path: 'contracts', loadChildren: './modules/contracts/contracts.module#ContractsModule' },
+      { path: 'inspection-insights', loadChildren: './modules/inspection-insights/inspection-insights.module#InspectionInsightsModule' },
+      { path: 'inspections', loadChildren: './modules/inspections/inspections.module#InspectionModule' },
+      { path: 'performance-insights', loadChildren: './modules/performance-insights/performance-insights.module#PerformanceInsightsModule' },
+      { path: 'tags', loadChildren: './modules/tags/tags.module#TagsModule' },
+      { path: 'templates', loadChildren: './modules/templates/templates.module#TemplatesModule' },
+      { path: 'users', loadChildren: './modules/users/users.module#UsersModule' },
+      { path: 'venues', loadChildren: './modules/venues/venues.module#VenuesModule' }
     ]
   }
 ];
