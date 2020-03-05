63b56a8451483c3150494e23557e3e0c13dd159e
RFuertes <reynelfuertes@gmail.com>
2020-03-05 21:45:57 +0800

    add missing declaration

--- src/app/modules/performance-insights/components/performance-insights-graphs/performance-insights-graphs.component.ts
+++ src/app/modules/performance-insights/components/performance-insights-graphs/performance-insights-graphs.component.ts
@@ -25,6 +25,7 @@ export class PerformanceInsightsGraphsComponent implements OnInit {
       }],
     }
   };
+  public lineChartPlugins: any;
   public barChartLabels: Label[] = [
     '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
     '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22',
