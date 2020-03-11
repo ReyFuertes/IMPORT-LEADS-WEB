b959b7fa8dbac94e2cc4987038700f80fa2ba37a
nick7576 <cybernick75@gmail.com>
2020-03-11 09:51:22 +0800

    numbers only with comma and dot

--- src/app/shared/directives/number-only.directive.ts
+++ src/app/shared/directives/number-only.directive.ts
@@ -5,31 +5,15 @@ import { NgControl } from '@angular/forms';
   selector: 'input[numbersOnly]'
 })
 export class NumberOnlyDirective {
-  @Input('decimals') decimals: number = 0;
-  private specialKeys = [ 'Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Delete', 'Comma' ];
-
-  constructor(private el: ElementRef) {
-  }
+  private specialKeys = [ 'Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Delete', ',' , '.' ];
+  constructor() {}
 
   @HostListener('keydown', [ '$event' ])
   onKeyDown(event: KeyboardEvent) {
-    if (this.specialKeys.indexOf(event.key) !== -1) {
+    const enteredKey = event.key;
+    if (this.specialKeys.indexOf(enteredKey) !== -1 || isNaN(Number(enteredKey)) === false) {
       return;
     }
-    const current: string = this.el.nativeElement.value;
-    const next: string = current.concat(event.key);
-    if ( (next && !this.check(next, this.decimals))) {
-      event.preventDefault();
-    }
+    event.preventDefault();
   }
-
-  private check(value: string, decimals: number) {
-    if (decimals <= 0) {
-      return String(value).match(new RegExp(/^\d+$/));
-    } else {
-        const regExpString = '^\\s*((\\d+(\\.\\d{0,' + decimals + '})?)|((\\d*(\\.\\d{1,' + decimals + '}))))\\s*$';
-        return String(value).match(new RegExp(regExpString));
-    }
-  }
-
 }
