81f76e31e12a712fa7288433f88815dbb9306464
RFuertes <reynelfuertes@gmail.com>
2020-03-25 22:41:10 +0800

    Fix verification remarks

--- src/app/modules/inspections/components/inspection-quality-requirement/inspection-quality-requirement.component.ts
+++ src/app/modules/inspections/components/inspection-quality-requirement/inspection-quality-requirement.component.ts
@@ -4,39 +4,13 @@ import { InspectionCommentDialogComponent } from './../../../dialogs/components/
 import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
+  id?: number,
   conditions: string;
   position: number;
   verification: Array<{ label: string, value: boolean }>;
   remarks: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1, conditions: 'All parts shall be in color and materials as shown in the quotation.',
-    verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
-    remarks: ''
-  },
-  {
-    position: 2, conditions: 'No bad physical condition.',
-    verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
-    remarks: ''
-  },
-  {
-    position: 3, conditions: 'No damages, cracks or scratches.',
-    verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
-    remarks: ''
-  },
-  {
-    position: 4, conditions: 'No bad physical condition.',
-    verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
-    remarks: ''
-  },
-  {
-    position: 5, conditions: 'No damages, cracks or scratches.',
-    verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
-    remarks: ''
-  },
-];
 @Component({
   selector: 'il-inspection-quality-requirement',
   templateUrl: './inspection-quality-requirement.component.html',
@@ -45,14 +19,49 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class InspectionQualityRequirementComponent implements OnInit {
   public displayedColumns: string[] = ['position', 'conditions', 'verification', 'remarks'];
-  public dataSource = ELEMENT_DATA;
+  public dataSource: PeriodicElement[] = [
+    {
+      id: 1, position: 1, conditions: 'All parts shall be in color and materials as shown in the quotation.',
+      verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
+      remarks: ''
+    },
+    {
+      id: 2, position: 2, conditions: 'No bad physical condition.',
+      verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
+      remarks: ''
+    },
+    {
+      id: 3, position: 3, conditions: 'No damages, cracks or scratches.',
+      verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
+      remarks: ''
+    },
+    {
+      id: 4, position: 4, conditions: 'No bad physical condition.',
+      verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
+      remarks: ''
+    },
+    {
+      id: 5, position: 5, conditions: 'No damages, cracks or scratches.',
+      verification: [{ label: 'Ok', value: true }, { label: 'Failed', value: false }, { label: 'Comment', value: false }],
+      remarks: ''
+    },
+  ];;
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() { }
 
-  public onRemarks(): void {
+  public onRemarks(items: PeriodicElement, idx: number): void {
+    items && items.verification.forEach((item, i) => {
+      if (i === idx) item.value = true
+      else item.value = false;
+    });
+    if(idx === 0) return;
     const dialogRef = this.dialog.open(InspectionCommentDialogComponent, {});
     dialogRef.afterClosed().subscribe(result => { });
+  }
+
+  private updateData(): void {
+
   }
 
   public showComment(): void {
